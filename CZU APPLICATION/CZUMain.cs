07c9839e8e0247cb6f7af8f18c0a318eed30988b
Gog Andrei <gogandrei2020@gmail.com>
2021-11-19 22:17:29 +0100

    added a new function to show just the connected users.

--- CZU APPLICATION/CZUMain.cs
+++ CZU APPLICATION/CZUMain.cs
@@ -28,12 +28,13 @@ namespace CZU_APPLICATION
             connectedId.Text = _connectedUser;
             _command = "select connected from users where connected =";
             Statistics.update(ref statisticsUsers, _command, "on");
-            Statistics.updateStudentPanel(ref studentGB1, ref studentImage1, ref studentConnected1, ref studentName1, ref studentQuestion1, ref studentAssignment1, ref studentMeeting1); // Need 6 here
-
         }   
+
 
         private void studentsButton_Click(object sender, EventArgs e)
         {
+            Statistics.updateStudentPanel(ref studentGB1, ref studentImage1, ref studentConnected1, ref studentName1, ref studentQuestion1, ref studentAssignment1, ref studentMeeting1); // Need 6 here
+            Statistics.updateConnectedStudents(studentPanel1, studentPanel2, studentPanel3, studentPanel4, studentPanel5, studentPanel6);
             studentTabState(true);
 
         }
@@ -76,8 +77,48 @@ namespace CZU_APPLICATION
             userDetails.Show();
         }
 
-     
+        private void studentConnected3_Click(object sender, EventArgs e)
+        {
 
+        }
+
+        private void studentImage1_Click_1(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+           
+        }
+
+        private void studentImage2_Click(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+        }
+
+        private void studentImage3_Click(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+        }
+
+        private void studentImage4_Click(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+
+        }
+
+        private void studentImage5_Click(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+        }
+
+        private void studentImage6_Click(object sender, EventArgs e)
+        {
+            CZUUserDetails studentDetails = new CZUUserDetails();
+            studentDetails.Show();
+        }
 
         private void studentTabState(Boolean mode)
         {
